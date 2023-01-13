3c08b7fde6cae0c12bb801ad2fa9c7ac19de3c4e
Arvind Subramanian <arvindosub@gmail.com>
2023-01-13 14:36:20 +0800

    Updates

---
+++ precache-manifest.d0bf2fe927e5af94bd8bc8268c205c67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e79389a99252fd8d4865",
+    "url": "/tokenfarm/static/css/main.b34d34c5.chunk.css"
+  },
+  {
+    "revision": "e79389a99252fd8d4865",
+    "url": "/tokenfarm/static/js/main.e79389a9.chunk.js"
+  },
+  {
+    "revision": "bd286bcaddbcc3ce3071",
+    "url": "/tokenfarm/static/css/1.1848fafe.chunk.css"
+  },
+  {
+    "revision": "bd286bcaddbcc3ce3071",
+    "url": "/tokenfarm/static/js/1.bd286bca.chunk.js"
+  },
+  {
+    "revision": "32ac716af363892fdd91",
+    "url": "/tokenfarm/static/js/runtime~main.32ac716a.js"
+  },
+  {
+    "revision": "3c2bdeae7a0db175dc21e4bb9d487f2a",
+    "url": "/tokenfarm/static/media/farmer.3c2bdeae.jpg"
+  },
+  {
+    "revision": "af16d37533d1c3ddea96869f7de47ba7",
+    "url": "/tokenfarm/static/media/lp.af16d375.png"
+  },
+  {
+    "revision": "f0fd37080eff0829b640cf2b0e86b875",
+    "url": "/tokenfarm/index.html"
+  }
+];
